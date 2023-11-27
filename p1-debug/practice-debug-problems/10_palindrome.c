709e6a09706e2c90671c95b76019c1c6d104cfc8
navya390 <151984829+navya390@users.noreply.github.com>
2023-11-27 06:44:00 +0530

    Update 10_palindrome.c

--- p1-debug/practice-debug-problems/10_palindrome.c
+++ p1-debug/practice-debug-problems/10_palindrome.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
-int check_palindrome(int arr, int n){ 
+int check_palindrome(int arr[], int n) {
     int is_palindrome = 1;
-    for (int i=0;i<n;i++){
-        if (arr[i] == arr[n-i]){ 
+    for (int i = 0; i < n / 2; i++) {
+        if (arr[i] != arr[n - i - 1]) {
             is_palindrome = 0;
+            break;  
         }
     }
     return is_palindrome;
 }
 
-int main(){
+int main() {
     int n;
-    scanf("%d", &n); // &n
+    scanf("%d", &n);
     int a[n];
-    for (int i=0 ;i<n; i++){
-        scanf("%d", &a[i]); // &a[i]
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
+
+    if (check_palindrome(a, n)) {
+        printf("Palindrome");
+    } else {
+        printf("Not Palindrome");
+    }
+
+    return 0;
 }
