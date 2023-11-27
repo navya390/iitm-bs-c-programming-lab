5cca8ae11c42bd33286fa9cdd566518161fc5cd1
navya390 <151984829+navya390@users.noreply.github.com>
2023-11-27 06:50:15 +0530

    Update 12_check_sorted.c

--- p1-debug/practice-debug-problems/12_check_sorted.c
+++ p1-debug/practice-debug-problems/12_check_sorted.c
@@ -2,9 +2,10 @@
 
 int check_sorted(int arr, int n){
     int is_sorted = 1;
-    for (int i=0; i<n; i++){ 
+    for (int i=0; i<n-1; i++){ 
         if (arr[i] > arr[i+1]){ 
             is_sorted = 0;
+            break;
         }
     }
     return is_sorted;
@@ -12,11 +13,11 @@ int check_sorted(int arr, int n){
 
 int main(){
     int n;
-    scanf("%d", n); 
+    scanf("%d", &n); 
     int a[n];
     for (int i=0; i<n; i++){
         scanf("%d", &a[i]);
     }
     int result = check_sorted(a, n);
-    printf("%s", result ? 'Sorted' : 'Not Sorted');
+    printf("%s", result ? "Sorted" : "Not Sorted");
 }
