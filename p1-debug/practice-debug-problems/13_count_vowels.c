77b8af6ab80f99adc02a82a17f42aae9a2b38866
navya390 <151984829+navya390@users.noreply.github.com>
2023-11-27 06:53:20 +0530

    Update 13_count_vowels.c

--- p1-debug/practice-debug-problems/13_count_vowels.c
+++ p1-debug/practice-debug-problems/13_count_vowels.c
@@ -4,13 +4,13 @@
 int count_vowels(char* str) {
     int count = 0;
     int n = strlen(str);
-    char vowels[] = {"a", "e", "i", "o", "u"};
+    char vowels[] = {'a', 'e', 'i', 'o', 'u'};
     
     for (int i=0; i<n; i++) {
         for (int j=0; j<5; j++) {
-            if (str[i] = vowels[j]) { 
+            if (str[i] == vowels[j]) { 
                 count++;
-                continue; 
+                break; 
             }
         }
     }
@@ -25,4 +25,5 @@ int main() {
     
     int result = count_vowels(s);
     printf("Number of vowels: %d", result);
+    return 0;
 }
