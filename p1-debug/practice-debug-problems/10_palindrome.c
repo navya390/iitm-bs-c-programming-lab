69f7336684e986ea232d7fb85fd0155e0a0f9de8
navya390 <151984829+navya390@users.noreply.github.com>
2023-11-27 06:38:44 +0530

    Update 10_palindrome.c

--- p1-debug/practice-debug-problems/10_palindrome.c
+++ p1-debug/practice-debug-problems/10_palindrome.c
@@ -12,9 +12,9 @@ int check_palindrome(int arr, int n){
 
 int main(){
     int n;
-    scanf("%d",n); // &n
+    scanf("%d", &n); // &n
     int a[n];
-    for (int i=0;i<n;++){
-        scanf("%d", a[i]); // &a[i]
+    for (int i=0 ;i<n; i++){
+        scanf("%d", &a[i]); // &a[i]
     }
 }
