81d1f9bd550acf9958897d27b3055cb3986f7ef8
navya390 <151984829+navya390@users.noreply.github.com>
2023-11-28 06:56:07 +0530

    Update 4_template_print.c

--- p2-template/4_template_print.c
+++ p2-template/4_template_print.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX_STUDENTS 100
 #define MAX_STRING   200
 
 typedef struct {
     // Following members needed in struct:
-    // - name: string
-    // - roll number: string
-    // - marks: integer
-    // - grade: char
+    char name[MAX_STRING];// - name: string
+    char rollno[MAX_STRING];// - roll number: string
+    int marks;// - marks: integer
+    char grade;// - grade: char
 
 } student_t;
 
@@ -16,49 +17,56 @@ void print_student(student_t s) {
     // Nitin C (abc123) got grade D with 50 marks
 
     // Fill in the correct info for the "_" values and the "..."
-    printf("%_ (%_) for grade %_ with %_ marks\n", ...);
+    printf("%s (%s) got grade %c with %d marks\n", .name, .rollno, .grade, .marks);
 }
 
 void compute_grade(student_t* sptr) {
     if (sptr->marks >= 90) sptr->grade = 'S';
-    else if 
-    else if
-    else ;
+    else if (sptr->marks >= 80) sptr->grade = 'A';
+    else if (sptr->marks >= 70) sptr->grade = 'B';
+    else if (sptr->marks >= 60) sptr->grade = 'C';
+    else if (sptr->marks >= 50) sptr->grade = 'D';
+    else if (sptr->marks >= 40) sptr->grade = 'E';
+    else sptr->grade = 'U';
 }
 
 int read_csv(char* filename, student_t s_array[MAX_STUDENTS]) {
     // Open a file for reading
     FILE* fp = fopen(filename, "r");
-    int num = 0;
+    if (fp==NULL){
+        printf("Failed to open file\n");
+    return 0;
+    }
 
+    int num = 0;
     char buf[MAX_STRING];
-    while (fgets(buf, MAX_STRING, fp) != NULL) {
+    while (fgets(buf, MAX_STRING, fp) != NULL && num < MAX_STUDENTS) {
         student_t* sptr = &s_array[num];
-        sscanf(buf, "%[^,],%[^,],%d", &(sptr->name), &(sptr->rollno), &(sptr->marks));
+        sscanf(buf, "%[^,],%[^,],%d", sptr->name, sptr->rollno, sptr->marks);
         compute_grade(sptr);
         num ++;
     }
-    return ...; 
+    fclose(fp);
+    return num;
 }
-
-// Create a filename rollno.txt (example: abc123.txt) and write the template there
 void tmpl_print(student_t s) {
     char tmpl[] = "To,\n"
-"%s (roll no. %s)\n"
-"Student of BS-ES course, IIT Madras\n"
-"\n"
-"Dear %s,\n"
-"\n"
-"You scored %d marks in the exam, and your grade is %c.\n"
-"\n"
-"Sincerely,\n"
-"The Dean\n";
+                  "%s (roll no. %s)\n"
+                  "Student of BS-ES course, IIT Madras\n"
+                  "\n"
+                  "Dear %s,\n"
+                  "\n"
+                  "You scored %d marks in the exam, and your grade is %c.\n"
+                  "\n"
+                  "Sincerely,\n"
+                  "The Dean\n";
 
     char filename[MAX_STRING];
-    sprintf(filename, "%s.txt", ...); 
+    sprintf(filename, "%s.txt", s.name);
     FILE* fp = fopen(filename, "w");
 
-    fprintf(fp, tmpl, ...);
+    fprintf(fp, tmpl, s.name, s.rollno, s.name, s.marks, s.grade);
+    fclose(fp);
 }
 
 int main() {
@@ -66,7 +74,8 @@ int main() {
     
     int num = read_csv("students.csv", s_array);
 
-    for () {
-        tmpl_print(s);
+    for (int i = 0; i < num; i++) {
+        print_student(s_array[i]);
     }
+    return 0;
 }
